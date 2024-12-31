49cec3439500ee1eb29ad8775d0937776de05b6d
Teddy Archibald <107772813+teddyvere@users.noreply.github.com>
2024-12-31 15:42:27 -0500

    main updated

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, world!'
+def index():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Heroku assigns the port dynamically
     app.run(host="0.0.0.0", port=port)
-
